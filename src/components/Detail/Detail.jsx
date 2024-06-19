06fa06fcc7327864d719bdd18ed58d6994e3eac3
mini-woong <mini_0@inu.ac.kr>
2024-06-19 09:52:57 +0900

    feature:  기능 구현 완료

--- src/components/Detail/Detail.jsx
+++ src/components/Detail/Detail.jsx
@@ -1,23 +1,37 @@
-import { Map } from "react-kakao-maps-sdk"
-//import useKakaoLoader from "./useKakaoLoader"
+import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import { fetchItem } from "../../api/pharmacy";
 
 export default function Detail() {
-  useKakaoLoader()
+  const { detailId } = useParams();
+
+  const {
+    data: pharmacy,
+    isLoading,
+    error
+  } = useQuery({
+    queryKey: ["pharmacy", detailId],
+    queryFn: fetchItem
+  });
+
+  if (isLoading) return <p>Loading...</p>;
+  if (error) return <p>Error fetching pharmacy</p>;
 
   return (
-    <Map // 지도를 표시할 Container
-      id="map"
-      center={{
-        // 지도의 중심좌표
-        lat: 33.450701,
-        lng: 126.570667,
-      }}
-      style={{
-        // 지도의 크기
-        width: "100%",
-        height: "350px",
-      }}
-      level={3} // 지도의 확대 레벨
-    />
-  )
+    <div>
+      <Map
+        center={{ lat: pharmacy[0].lat, lng: pharmacy[0].lon }}
+        style={{ width: '800px', height: '600px' }}
+        level={3}
+      >
+        <MapMarker position={{ lat: pharmacy[0].lat, lng: pharmacy[0].lon }} />
+      </Map>
+      <div>
+        <p>{pharmacy[0]['place-name']}</p>
+        <p>{pharmacy[0]['address']}</p>
+        <p>전화번호 : {pharmacy[0]['phone-number']}</p>
+      </div>
+    </div>
+  );
 }
