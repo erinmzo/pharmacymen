8ef52ced1f563c393fd87b186c612f2fd6848f80
SNGCHN <sngchn0312@naver.com>
2024-06-19 19:22:32 +0900

    fix: 파일 이름 수정

--- src/components/Login/LoginForm.jsx
+++ src/components/Login/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { loginUser } from "../../api/auth";
-import useAuthStore from "../../zustand/auth";
+import useAuthStore from "../../zustand/Auth";
 
 function LoginForm() {
 	const navigate = useNavigate();
